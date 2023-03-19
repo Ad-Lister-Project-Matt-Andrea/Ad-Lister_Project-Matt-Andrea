4f4d7038b1946ecc5fa9fb35b9cc1309354c8386
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-03-19 12:22:22 -0500

    bootstrap styling

--- src/main/webapp/WEB-INF/js/user_validation.js
+++ src/main/webapp/WEB-INF/js/user_validation.js
@@ -1,18 +1,64 @@
 (function () {
     'use strict'
 
-    var forms = document.querySelectorAll('.needs-validation')
+    const forms = document.querySelectorAll('.needs-validation');
 
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
+    //validate username
+    $("#usernameInvalid").hide();
+    let usernameError = true;
+    $("#username").keyup(function(){
+        validateUsername();
+    })
+    function validateUsername(){
+        let username = $("#username").val();
+        if(username.length === ""){
+            $("#usernameInvalid").show();
+            usernameError = false;
+            return false;
+        } else if (username.length < 3 ){
+            $("#usernameValid").show();
+            usernameError = false;
+            return false;
+        } else{
+            $("#usernameInvalid").hide();
+        }
+    }
+    //validate password
+    $("#passwordInvalid").hide();
+    let passwordError = true;
+    $("#password").keyup(function(){
+        validatePassword();
+    })
+    function validatePassword(){
+        let password = $("#password").val();
+        if(password.length === ""){
+            $("#passwordInvalid").show();
+            passwordError = false;
+            return false;
+        } else if (password.length < 3 ){
+            $("#passwordValid").show();
+            passwordError = false;
+            return false;
+        } else{
+            $("#passwordInvalid").hide();
+        }
+    }
+
+    //submit button
+    // Array.prototype.slice.call(forms)
+    //     .forEach(function (form) {
+    //         form.addEventListener('submit', function (event) {
+    $("#submitbtn").click(function(){
+                validateUsername();
+                validatePassword();
+                if (usernameError == true && passwordError == true) {
+                    return true;
+                }else{
+                    return false;
                 }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
+                // form.classList.add('was-validated');
 
+            // }, false)
+        })
 })()
